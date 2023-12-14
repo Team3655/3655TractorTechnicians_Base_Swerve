6a42a7594c58f8f5df2e08bb2e5de56ed187726c
quinnhaygood <62080573+quinnhaygood@users.noreply.github.com>
2023-12-13 23:25:03 -0500

    release the kraken

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "3655TractorTechnicians_Base_Swerve";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 47;
-  public static final String GIT_SHA = "4624a212b971e964d53d1d5a85ad178f8ed6ba1d";
-  public static final String GIT_DATE = "2023-12-13 15:47:50 EST";
+  public static final int GIT_REVISION = 49;
+  public static final String GIT_SHA = "e680951ebc3a2dab26a9ce5f90da892203db205b";
+  public static final String GIT_DATE = "2023-12-13 22:51:14 EST";
   public static final String GIT_BRANCH = "AdvantageKit_Attempt";
-  public static final String BUILD_DATE = "2023-12-13 22:12:33 EST";
-  public static final long BUILD_UNIX_TIME = 1702523553321L;
+  public static final String BUILD_DATE = "2023-12-13 23:08:10 EST";
+  public static final long BUILD_UNIX_TIME = 1702526890410L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
