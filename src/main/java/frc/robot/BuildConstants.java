d18d41de1be0092a17bab38e6db4f523d7e3f947
quinnhaygood <62080573+quinnhaygood@users.noreply.github.com>
2023-12-21 14:08:22 -0500

    updates to drive constants

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "3655TractorTechnicians_Base_Swerve";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 53;
-  public static final String GIT_SHA = "e81ac165c72a4901ff0b244a11009924db2da6a5";
-  public static final String GIT_DATE = "2023-12-20 13:18:58 EST";
+  public static final int GIT_REVISION = 54;
+  public static final String GIT_SHA = "ae061d75a5e2ac0e564c23074444ba1bb19a1a7b";
+  public static final String GIT_DATE = "2023-12-20 14:14:22 EST";
   public static final String GIT_BRANCH = "AdvantageKit_Attempt";
-  public static final String BUILD_DATE = "2023-12-20 14:08:50 EST";
-  public static final long BUILD_UNIX_TIME = 1703099330901L;
+  public static final String BUILD_DATE = "2023-12-21 13:23:43 EST";
+  public static final long BUILD_UNIX_TIME = 1703183023179L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
